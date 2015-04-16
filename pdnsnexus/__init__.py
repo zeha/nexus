5d9dec5b6cfceb15c82dbe7c8b0ed0e31b365940
Christian Hofstaedtler <christian@hofstaedtler.name>
2015-04-16 22:20:07 +0200

    Don't always require a pdnsnexus.conf
    For now.

--- pdnsnexus/__init__.py
+++ pdnsnexus/__init__.py
@@ -1,6 +1,6 @@
 # (imagine ascii text art "nexus" here)
 
-from flask import Flask
+from flask import Flask, current_app
 from flask.ext.security import Security, current_user
 import os
 
@@ -13,26 +13,31 @@ class Control(Flask):
 app = Control(__name__, instance_relative_config=True)
 
 app.config.from_object('pdnsnexus.default_settings')
-app.config.from_pyfile('pdnsnexus.conf')
+
+# app.config.from_pyfile('pdnsnexus.conf')
 
 app.config['SECURITY_TRACKABLE'] = True
 app.config['SECURITY_CHANGEABLE'] = True
 app.config['SECURITY_URL_PREFIX'] = '/auth'
 app.config['SECURITY_CHANGE_URL'] = '/change-password'
 app.config['SECURITY_SEND_PASSWORD_CHANGE_EMAIL'] = False
-
 app.config['SQLALCHEMY_DATABASE_URI'] = app.config['DATABASE_URI']
 
-if not app.config['DATABASE_URI']:
-    raise Exception('DATABASE_URI must be set in pdnsnexus.conf.')
 
+def verify_config():
+    if not current_app.config['DATABASE_URI']:
+        raise Exception('DATABASE_URI must be set in pdnsnexus.conf.')
+
+
+app.verify_config = verify_config
 
 
 @app.errorhandler(404)
 def not_found(error):
     return 'Not found', 404
 
-import .api
+
+from . import api
 app.register_blueprint(api.mod, url_prefix='/api')
 
 from .models import user_datastore
